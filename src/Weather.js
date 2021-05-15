eb79abaa5b4b0c0b7f78dc85dc76a91f195a3292
Radika <radikakumar@gmail.com>
2021-05-15 01:20:03 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -27,6 +27,7 @@ function Search(){
   let key= "2f9f7ec47cab1795a041f2ec45034bf2"
   let Url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
   axios.get(Url).then(displayWeather);
+  setMessage(Dataweather.city);
   
    
 }
@@ -38,7 +39,7 @@ function handleSubmit(event){
 
 function updateCity(event){
   setCity(event.target.value);
-  setMessage(Dataweather.city); 
+   
 }
 
 if (Ready) {return (
