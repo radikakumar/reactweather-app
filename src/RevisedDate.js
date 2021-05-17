1ef9c2aae6f9e6733d31dd6ce661a02898e4cbf6
Radika <radikakumar@gmail.com>
2021-05-17 19:45:22 +0100

    Update RevisedDate.js

--- src/RevisedDate.js
+++ src/RevisedDate.js
@@ -1,18 +1,18 @@
 import React from "react";
 
 export default function RevisedDate(props){
-   let Days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-   let Month=["January, February", "March", "April", 
+   var Days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   var Month=["January, February", "March", "April", 
    "May", "June","July","August", "September", "October", "November", "December"];
     
-   let Day=Days[props.date.getDay()];
-   let Date=props.date.getDate();
-   let Hours=props.date.gethours();
+   var Day=Days[props.date.getDay()];
+   var Date=props.date.getDate();
+   var Hours=props.date.gethours();
    if (Hours<10){ Hours=`0${Hours}`};
-   let Minutes=props.date.getMinutes();
+   var Minutes=props.date.getMinutes();
    if (Minutes<10){Minutes=`0${Minutes}`;
-   let Months=Month[props.getMonth()];
-   let Year=props.date.getFullYear();
+   var Months=Month[props.getMonth()];
+   var Year=props.date.getFullYear();
     return (
      <div>
      `🗓️{Day},{Date},{Hours}:{Minutes}{Months}{Year} `
