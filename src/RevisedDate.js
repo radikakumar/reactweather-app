e7c10ac820ae6dc1d87455403d3bfba49959f995
Radika <radikakumar@gmail.com>
2021-05-17 20:34:45 +0100

    Update RevisedDate.js

--- src/RevisedDate.js
+++ src/RevisedDate.js
@@ -1,22 +1,23 @@
 import React from "react";
 
 export default function RevisedDate(props){
-   var Days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-   var Month=["January, February", "March", "April", 
-   "May", "June","July","August", "September", "October", "November", "December"];
-    
-   var Day=Days[props.date.getDay()];
-   var Date=props.date.getDate();
-   var Hours=props.date.gethours();
-   if (Hours<10){ Hours=`0${Hours}`};
-   var Minutes=props.date.getMinutes();
-   if (Minutes<10){Minutes=`0${Minutes}`;
-   var Months=Month[props.getMonth()];
-   var Year=props.date.getFullYear();
+let Year = props.date.getFullYear();
+let days=["Sun","Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+let Day = days[props.date.getDay()];
+let months=["Jan", "Feb", "March", "April", "May", "June","July", "Aug", "Sept", "Oct", "Nov","Dec"];
+let Month= months[props.date.getMonth()];
+let Date= props.date.getDate();
+let Hour=props.date.getHours();
+if (Hour <10)
+{Hour= (`0${Hour}`)}
+let Minutes=props.date.getMinutes();
+if (Minutes <10) {Minutes =(`0${Minutes}`)}
+let Seconds=props.date.getSeconds();
+if (Seconds<10) {Seconds= (`0${Seconds}`)}
     return (
      <div>
-     `🗓️{Day},{Date},{Hours}:{Minutes}{Months}{Year} `
+     `🗓️{Day},{Date},{Hour}:{Minutes}:{Seconds}{Month}{Year} `
      </div>
     );
    }
-   }
+   
