61fb3569d3707908f9e6d3916aa4580271a53df7
Radika <radikakumar@gmail.com>
2021-05-23 20:35:21 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ export default function App() {
      <div>
        <Weather defaultCity="Paris"/>
      </div>
-    </div>
+     <div className="Footer">
+      This is an <a href="https://github.com/radikakumar">open source code</a> app developed by <a href="https://www.linkedin.com/in/radika-kumar-989b3944/">Radika Kumar</a>
+      
+       
+       </div>
+     </div>
+    
+
   );
 }
 
